a164e9c2b4fd2fa1dabec82b0a1eda1eb76644f7
Likhitha Reddy Yatham <chandralekhayatham@gmail.com>
2026-05-31 16:21:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Dashboard />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
